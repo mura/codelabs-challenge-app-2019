13caa9f1141a602fa166fcdb60fa9c21279e6987
Yohei Murayama <yomuraya@yahoo-corp.jp>
2019-02-08 13:12:11 +0900

    add RxJava

--- app/src/main/java/droidkaigi/github/io/challenge2019/BaseActivity.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/BaseActivity.kt
@@ -9,6 +9,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -30,6 +31,7 @@ abstract class BaseActivity : AppCompatActivity() {
         return Retrofit.Builder()
             .baseUrl(url)
             .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
     }
 
--- app/src/main/java/droidkaigi/github/io/challenge2019/data/api/HackerNewsApi.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/data/api/HackerNewsApi.kt
@@ -2,6 +2,7 @@ package droidkaigi.github.io.challenge2019.data.api
 
 import droidkaigi.github.io.challenge2019.data.api.response.Item
 import droidkaigi.github.io.challenge2019.data.api.response.User
+import io.reactivex.Single
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -10,11 +11,17 @@ interface HackerNewsApi {
     @GET("item/{id}.json")
     fun getItem(@Path("id") id: Long): Call<Item>
 
+    @GET("item/{id}.json")
+    fun getItemAsSingle(@Path("id") id: Long): Single<Item>
+
     @GET("user/{id}.json")
     fun getUser(@Path("id") id: String): Call<User>
 
     @GET("topstories.json")
     fun getTopStories(): Call<List<Long>>
+
+    @GET("topstories.json")
+    fun getTopStoriesAsSingle(): Single<List<Long>>
 
     @GET("newstories.json")
     fun getNewStories(): Call<List<Long>>
